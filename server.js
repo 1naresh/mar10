feb737905626970c2c1e693936650415b355ac20
1naresh <mergu345@gmail.com>
2018-03-11 11:44:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+var express=require('express')
+var fs=require('fs')
+var bodyParser=require('body-parser')
+var mongoose=require('mongoose')
+var data=require('./routes/data')
+var scripts=require('./routes/scripts')
+var templates=require('./routes/templates')
+var app=express();
+
+mongoose.Promise=global.Promise;
+mongoose.connect('mongodb://localhost/test')
+
+app.get('/index',function(req,res){
+    fs.readFile('src/html/index.html','utf8',function(err,data){
+        res.send(data)
+    })
+})
+app.get('/style.css',function(req,res){
+    fs.readFile('src/css/style.css','utf8',function(err,data){
+        res.send(data)
+    })
+})
+app.get('/app.js',function(req,res){
+    fs.readFile('src/js/app.js','utf8',function(err,data){
+        res.send(data)
+    })
+})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/data',data)
+app.use('/js',scripts)
+app.use('/templates',templates)
+app.listen(100,function(){
+    console.log('server is listening')
+})
